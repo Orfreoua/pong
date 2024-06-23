a838b405a0ee025e6aac07fb5ab1d61f7493b5e5
Oren FREOUA <orfreoua@bess-f1r1s13.clusters.42paris.fr>
2024-06-23 14:56:14 +0200

    update

--- test/test/script.js
+++ test/test/script.js
@@ -4,15 +4,23 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const particlesArray = [];
-const numberOfParticles = 100;
+const numberOfParticles = 150;
+
+// Color gradients for the fire effect
+const colors = [
+    'rgba(255, 69, 0, 0.8)', // red-orange
+    'rgba(255, 140, 0, 0.7)', // orange
+    'rgba(255, 215, 0, 0.6)'  // yellow
+];
 
 class Particle {
     constructor() {
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
         this.size = Math.random() * 5 + 1;
-        this.speedX = Math.random() * 3 - 1.5;
-        this.speedY = Math.random() * 3 - 1.5;
+        this.speedX = (Math.random() - 0.5) * 1;
+        this.speedY = Math.random() * -2 - 1; // Always moving upwards
+        this.color = colors[Math.floor(Math.random() * colors.length)];
     }
     update() {
         this.x += this.speedX;
@@ -20,7 +28,9 @@ class Particle {
         if (this.size > 0.2) this.size -= 0.1;
     }
     draw() {
-        ctx.fillStyle = 'rgba(255, 69, 0, 0.8)';
+        ctx.fillStyle = this.color;
+        ctx.shadowColor = 'rgba(0, 0, 0, 0.5)';
+        ctx.shadowBlur = 10;
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.closePath();
@@ -33,6 +43,7 @@ function init() {
         particlesArray.push(new Particle());
     }
 }
+
 function handleParticles() {
     for (let i = 0; i < particlesArray.length; i++) {
         particlesArray[i].update();
